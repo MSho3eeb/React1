78cb47e5bbd8baf2de373d1677422d8fbcdd84fb
Mohamed Shoaib <mohamedshoaib307@gmail.com>
2025-11-22 22:09:14 +0200

    removing initial search and fixed a warning

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     }
 
     useEffect(() => {
-        searchMovie('Spiderman')
+        searchMovie('')
     }, [])
 
     return (
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -7,7 +7,7 @@ const MovieCard = ({movie}) => {
             <p>{movie.Year}</p>
         </div>
         <div>
-            <img src={movie.Poster != 'N/A' ? movie.Poster: 'https://via.placeholder.com/400'} alt={movie.title}/>
+            <img src={movie.Poster !== 'N/A' ? movie.Poster: 'https://via.placeholder.com/400'} alt={movie.title}/>
         </div>
         <div>
             <span>
